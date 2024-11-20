c8aae34e203719f0588634df8c32e1a5be603012
miyamakohki <kmx.111010@gmail.com>
2024-11-20 21:23:38 +0900

    不用なログを削除

--- src/test.py
+++ src/test.py
@@ -9,15 +9,12 @@ def test(model, x, y):
     bs = 1000
     test_pred = []
 
-    print(f"y_test: {y_test[0]}")
-
     for i in range(0, len(x_test), bs):
         batch_pred = model(x_test[i : i + bs])
         test_pred.append(batch_pred)
     test_pred = np.concatenate(test_pred, axis=0)
 
     test_pred = np.argmax(test_pred, axis=1)
-    print(f"y_pred: {test_pred[0]}")
 
     acc = accuracy_score(y_test, test_pred)
     print(f"Accuracy: {acc}")
