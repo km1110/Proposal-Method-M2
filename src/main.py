a9835481168654c0b2fc660bb6014affa07c81e3
miyamakohki <kmx.111010@gmail.com>
2024-11-30 08:14:26 +0900

    軽微な修正

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import argment
 
 warnings.filterwarnings("ignore")
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0, 3"
 
 import tensorflow as tf
 
@@ -18,7 +18,7 @@ from test import test
 def set_gpus():
     gpus = tf.config.experimental.list_physical_devices("GPU")
     tf.config.experimental.set_visible_devices(
-        [gpus[0]],  # , gpus[1], gpus[2]
+        [gpus[0], gpus[1]],  # , , gpus[2]
         "GPU",
     )
     strategy = tf.distribute.MirroredStrategy()
